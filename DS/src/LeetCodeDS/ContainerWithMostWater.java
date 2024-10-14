2c51bcba2056a3044725797dbbfdf27a10f5c7bf
Tarun Kashyap <tarunk05151999@gmail.com>
2024-10-14 20:06:28 +0530

    Leet Code DS

---
+++ DS/src/LeetCodeDS/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package LeetCodeDS;
+
+public class ContainerWithMostWater {
+    public static void main(String []args){
+        int [] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int maxArea = 0;
+        while(left<right){
+            maxArea = Math.max(maxArea, (right-left)*Math.min(height[left],height[right]));
+            if(height[left] < height[right]){ left++;
+            }
+            else { right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
+
---
+++ DS/src/LeetCodeDS/SlidingWindow_Easy.java
@@ -0,0 +1,40 @@
+package LeetCodeDS;
+
+public class SlidingWindow_Easy {
+    public static void main(String [] args){
+        int [] ar = {1,12,-5,-6,50,3};
+        System.out.println(findMaxAverage(ar,4));
+    }
+    public static double findMaxAverage(int[] nums, int k) {
+        double maxAvg = 0;
+        double sum = 0;
+
+        for(int i=0; i<k;i++){
+            sum+=nums[i];
+        }
+        maxAvg = sum/k;
+
+        //System.out.println(maxAvg);
+        for(int i=k;i<nums.length;i++){
+            sum = sum - nums[i-k] + nums[i];
+            maxAvg = Math.max(maxAvg,sum/k);
+        }
+        return maxAvg;
+    }
+
+    //2.0 & 49.5
+//        12.375 & 40.5
+//        12.375
+////    double ans
+// = 0;
+//    double window = 0;
+//        for(int i=0;i<k;i++) {
+//        window +=nums[i];
+//    }
+//    ans = window/k;
+//        for(int right=k;right<nums.length;right++) {
+//        window+=nums[right] - nums[right-k];
+//        ans = Math.max(ans,window/k);
+//    }
+//        return ans;
+}

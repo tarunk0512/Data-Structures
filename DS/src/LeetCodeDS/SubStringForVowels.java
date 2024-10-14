d904a862efb101e6082ae292de954f89b6418241
Tarun Kashyap <tarunk05151999@gmail.com>
2024-10-14 21:20:55 +0530

    Leet Code DS

---
+++ DS/src/LeetCodeDS/SubStringForVowels.java
@@ -0,0 +1,44 @@
+package LeetCodeDS;
+
+import java.util.Set;
+
+public class SubStringForVowels {
+    public static void main(String[] args){
+        String s = "leetcode";
+        int k = 3;
+        System.out.println(maxVowels(s,k));
+    }
+    public static int maxVowels(String s, int k) {
+        int max = 0;
+        int count = 0 ;
+        for(int i = 0; i< k; i++){
+            if(isVowel(s.charAt(i))){
+                count++;
+            }
+        }
+        if(count == k ) return k;
+        max = count;
+        int j = k;
+        for(int i = 0; i< s.length()-k; i++){
+            if(isVowel(s.charAt(i))){
+                count--;
+            }
+            if(isVowel(s.charAt(j))){
+                count++;
+            }
+            j++;
+
+            max = Math.max(count,max);
+            if(count == k) return k;
+        }
+        return max;
+    }
+    public static Boolean isVowel(Character s){
+        Set<Character> hset = Set.of('a','e','i','o','u');
+        if(hset.contains(s)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}

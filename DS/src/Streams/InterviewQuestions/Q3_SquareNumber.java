074a1ddf26e8191ecd2274e417ac11749b20810b
Tarun Kashyap <tarunk05151999@gmail.com>
2024-10-22 21:29:58 +0530

    Streams Q3

---
+++ DS/src/Streams/InterviewQuestions/Q3_SquareNumber.java
@@ -0,0 +1,28 @@
+package Streams.InterviewQuestions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q3_SquareNumber {
+    public static void main(String [] args){
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        List<Integer> square = list.stream()
+                .map(e -> e*e)
+                .filter(e -> e> 5)
+                        .collect(Collectors.toList());
+        //.toList();
+        //square.add(56);
+
+        double avg = list.stream()
+                        .map(n -> n*n)
+                                .filter(n -> n>15)
+                                        .mapToDouble(n ->n)
+                                                .average().getAsDouble();
+
+        System.out.println("Sqaure is : "+square);
+        System.out.println("Average is : "+ avg);
+
+
+    }
+}

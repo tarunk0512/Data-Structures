f185fd6fb5149f8cf4e09707b7c590e98a8184e8
Tarun Kashyap <tarunk05151999@gmail.com>
2024-11-11 21:19:31 +0530

    Streams Q9

---
+++ DS/src/Streams/InterviewQuestions/Q9_LimitGetIgnoreImpl.java
@@ -0,0 +1,29 @@
+package Streams.InterviewQuestions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q9_LimitGetIgnoreImpl {
+    public static void main(String [] args){
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,12,13,14,15);
+
+        List<Integer> l1 = list.stream().limit(5).toList();
+        System.out.println(l1);
+        //O/P : [1, 2, 3, 4, 5]
+
+        int sum = list.stream().limit(5).reduce(Integer::sum).get();
+        System.out.println(sum);
+        // O/P : 15
+
+        List<Integer> s1 = list.stream().skip(5).toList();
+        System.out.println(s1);
+        // O/P : [6, 7, 8, 9, 10, 12, 13, 14, 15]
+
+        int sumSkp = list.stream().skip(5).reduce(Integer :: sum).get();
+        System.out.println(sumSkp);
+        //O/P : 94  (Sum of all number : [6, 7, 8, 9, 10, 12, 13, 14, 15])
+
+
+    }
+}

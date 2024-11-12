ab857893cfb4520751be45b844be238679340f34
Tarun Kashyap <tarunk05151999@gmail.com>
2024-11-12 10:51:05 +0530

    LinkedList - Initial Commit

---
+++ DS/src/LinkedList/SingleLinkedList/Test.java
@@ -0,0 +1,13 @@
+package LinkedList.SingleLinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String [] args){
+        List<Integer> list = new ArrayList<>();
+        int n = 5;
+        list.add(n);
+        System.out.println();
+    }
+}

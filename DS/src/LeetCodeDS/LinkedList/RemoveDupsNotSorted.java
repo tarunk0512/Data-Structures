d120dfdd82530ff9a98bba079c580e16ed583cac
Tarun Kashyap <tarunk05151999@gmail.com>
2025-01-28 22:22:48 +0530

    LinkedList : Remove Duplicate - leetCode

--- DS/src/LeetCodeDS/LinkedList/RemoveDupsNotSorted.java
+++ DS/src/LeetCodeDS/LinkedList/RemoveDupsNotSorted.java
@@ -6,18 +6,19 @@ public class RemoveDupsNotSorted {
     public static void main(String [] args){
         ListNode head = new ListNode();
         ListNode curr = head;
-        int [] ar = {1,2,3,4,5,6,3};
+        int [] ar = {1,1,2,3,4,5,6,3};
         for(int i =0; i< ar.length; i++){
             curr.next = new ListNode(ar[i]);
             curr = curr.next;
         }
-        ListNode res = removeDups(head.next);
-        printList(res);
+        //ListNode res = removeDups(head.next);
+        ListNode res2 = removeNode(head, 1);
+        printList(res2);
     }
     public static ListNode removeDups(ListNode head){
         HashMap<Integer, Integer> map = new HashMap<>();
-        ListNode curr = head;
-        ListNode curr1 = head;
+        ListNode curr = new ListNode(0, head);
+        ListNode curr1 = new ListNode(0, head);
         ListNode prev = curr1;
         while(curr != null){
             map.put(curr.val, map.getOrDefault(curr.val, 0)+1);
@@ -36,6 +37,19 @@ public class RemoveDupsNotSorted {
         return head;
     }
 
+    public static ListNode removeNode(ListNode head, int val){
+        ListNode curr = new ListNode(0, head);
+        ListNode prev = curr;
+        while(curr != null){
+            if(curr.val == val){
+                prev.next = curr.next;
+            }else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return head.next;
+    }
 
     public static void printList(ListNode head){
         if(head == null) {

c70a20db0f7f4a8dd8ec8d12b21eed878cddc1cd
Tarun Kashyap <tarunk05151999@gmail.com>
2025-01-28 21:54:16 +0530

    LinkedList : Remove Duplicate - leetCode

--- DS/src/LeetCodeDS/LinkedList/RemoveDupsNotSorted.java
+++ DS/src/LeetCodeDS/LinkedList/RemoveDupsNotSorted.java
@@ -17,16 +17,20 @@ public class RemoveDupsNotSorted {
     public static ListNode removeDups(ListNode head){
         HashMap<Integer, Integer> map = new HashMap<>();
         ListNode curr = head;
-        ListNode prev = curr;
+        ListNode curr1 = head;
+        ListNode prev = curr1;
         while(curr != null){
             map.put(curr.val, map.getOrDefault(curr.val, 0)+1);
-            if(map.get(curr.val) > 1){
-                prev.next = curr.next;
+            curr = curr.next;
+        }
+        while(curr1 != null){
+            if(map.get(curr1.val) > 1){
+                prev.next = curr1.next;
             }else{
-                prev = curr;
+                prev = curr1;
                 System.out.print(prev.val);
             }
-            curr = curr.next;
+            curr1 = curr1.next;
         }
         System.out.println(map);
         return head;

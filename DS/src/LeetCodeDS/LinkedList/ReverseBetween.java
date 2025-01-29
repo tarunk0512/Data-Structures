9e2050f5b38ffbb4e2f7e50ea9635ab4e539614e
Tarun Kashyap <tarunk05151999@gmail.com>
2025-01-29 20:24:50 +0530

    LinkedList : Remove Duplicate - leetCode

--- DS/src/LeetCodeDS/LinkedList/ReverseBetween.java
+++ DS/src/LeetCodeDS/LinkedList/ReverseBetween.java
@@ -29,14 +29,20 @@ public class ReverseBetween {
         for(int i =0 ; i< left-1; i++){
             prev = prev.next;
         }
-
+//1 - 2 - 3 - 4 - 5
         ListNode curr = prev.next;
         for(int i =0; i<right-left; i++){
-            ListNode temp = curr.next;
-            curr.next = temp.next;
-            temp.next = prev.next;
-            prev.next = temp;
+            ListNode temp = curr.next; //temp -> 3 curr -> 2
+            //curr = 2, temp = 2.next == 4 -> temp =4
+            curr.next = temp.next; //curr =2 = 2->4
+            //2->5(temp/4.next)
+            temp.next = prev.next;//temp = 3 = 3->2
+            //4->3 => temp.next->3(prev.next)
+            prev.next = temp;//1->3
+            // 1(prev).next->4(temp)
         }
+        //After 1st iteration 1->3->2->4->5
+        //After 2nd iteration 1->4->3->2->5
         return dummy.next;
     }
 

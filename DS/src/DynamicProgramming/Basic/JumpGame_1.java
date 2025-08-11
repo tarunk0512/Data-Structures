1e7d163721e701bc4768585c82fca6b5e6edc431
Tarun Kashyap <tarunk05151999@gmail.com>
2025-08-11 22:58:13 +0530

    DP : Jump1

--- DS/src/DynamicProgramming/Basic/JumpGame_1.java
+++ DS/src/DynamicProgramming/Basic/JumpGame_1.java
@@ -1,5 +1,10 @@
 package DynamicProgramming.Basic;
-
+// Given an array of non-negative integers, where each element represents the maximum jump length at that position,
+ // determine if you can reach the last index starting from the first index.
+ // You can assume that you can always reach the last index if you start from the first index.
+ // EG : [2,3,1,1,4] -> true (can jump to 4) -> you'll jump from 2(index 0) to 1(index 2),
+// then index 2 to index 3, and finally index 3 to index 4
+ // EG : [3,2,1,0,4] -> false (cannot reach 4)
 public class JumpGame_1 {
     public static void main(String [] args){
         int [] jumps = new int[]{3,2,1,0,4};

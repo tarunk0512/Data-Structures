b55bd01ac4382d4a70b509d1eb7e185d9dd6e977
Tarun Kashyap <tarunk05151999@gmail.com>
2025-08-20 22:18:59 +0530

    DP : Strings : Longest Common Subsequence

---
+++ DS/src/DynamicProgramming/StringDP/LongestCommonSubsequence.java
@@ -0,0 +1,30 @@
+package DynamicProgramming.StringDP;
+
+public class LongestCommonSubsequence {
+    public static void main(String [] args){
+        String s1 = "ace";
+        String s2 = "abcdefg";
+        System.out.println(getLongestCommonSubsequence(s1,s2));
+
+    }
+
+    public static int getLongestCommonSubsequence(String s1, String s2){
+        int s1Length = s1.length();
+        int s2Length = s2.length();
+        int [][] dp = new int[s1Length+1][s2Length+1];
+
+        for(int i = 1; i<=s1Length; i++){
+            for(int j = 1; j<=s2Length; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+
+            }
+        }
+        return dp[s1Length][s2Length];
+
+    }
+
+}

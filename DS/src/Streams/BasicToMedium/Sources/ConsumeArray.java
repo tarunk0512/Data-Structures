5d9beba44ec5eb45ccc3617bf040aa7ed12a014e
Tarun Kashyap <tarunk05151999@gmail.com>
2025-09-22 20:41:49 +0530

    Streams - Array Consume

---
+++ DS/src/Streams/BasicToMedium/Sources/ConsumeArray.java
@@ -0,0 +1,19 @@
+package Streams.BasicToMedium.Sources;
+
+import java.util.Arrays;
+
+public class ConsumeArray {
+    public static void main(String [] args){
+        String [] strAr = {"hello", "my", "Name", "is", "tarun"};
+        Character [] charAr = {'a', 'b', 'c', 'd', 'e'};
+        int [] intAr = {1,2,3,4,5,6};
+
+        Arrays.stream(strAr).
+                forEach(System.out::println);
+
+        Arrays.stream(charAr).
+                forEach(System.out::println);
+
+        Arrays.stream(intAr).forEach(System.out::print);
+    }
+}

e80c7c2eae7c99d98b4ac14e1313293bb80a8b1d
Tarun Kashyap <tarunk05151999@gmail.com>
2025-09-23 22:26:35 +0530

    Streams - Terminal Ops - Reduce Functions

---
+++ DS/src/Streams/BasicToMedium/TerminalOps/ReduceOps.java
@@ -0,0 +1,20 @@
+package Streams.BasicToMedium.TerminalOps;
+
+import java.util.List;
+
+public class ReduceOps {
+    public static void main(String[] args){
+        // reduce → Combine all elements into a single result using the provided binary operation.
+        // It takes two parameters: an identity value and a BinaryOperator function.
+        // The identity value is the initial value for the reduction and also the default result if the
+        // stream is empty.
+        // The BinaryOperator function defines how two elements are combined.
+        List<Integer> list = List.of(1,2,3,4,5,6);
+        int sum = list.stream().reduce(0, (a,b) -> a+b);
+        // here 0 is the identity value and (a,b) -> a+b is the BinaryOperator function
+        // which adds two integers.
+        // So, the reduction process will start with 0 and then add each element of the stream to it.
+        // The final result will be the sum of all elements in the stream.
+        System.out.println("Sum is : "+sum);
+    }
+}

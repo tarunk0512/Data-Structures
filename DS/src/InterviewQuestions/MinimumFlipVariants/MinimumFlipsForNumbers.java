74b1834bb90deb3d866a3e6e6d462182878473aa
Tarun Kashyap <tarunk05151999@gmail.com>
2025-09-24 09:23:49 +0530

    Minimum FLips Required for nums

---
+++ DS/src/InterviewQuestions/MinimumFlipVariants/MinimumFlipsForNumbers.java
@@ -0,0 +1,33 @@
+package InterviewQuestions.MinimumFlipVariants;
+
+public class MinimumFlipsForNumbers {
+    public static void main(String [] args){
+        String s = "1111110000";
+        System.out.println(minFlipsRequired(s));
+
+    }
+
+    public static int minFlipsRequired(String s){
+        int n = s.length();
+        int count0 = 0;
+        int count1 = 0;
+
+        char prev = s.charAt(0);
+        if(prev == '0'){
+            count0++;
+        }else{
+            count1++;
+        }
+
+        for(int i = 1; i<n; i++){
+            if(s.charAt(i) == '0'){
+                count0++;
+            }else{
+                count1++;
+            }
+        }
+        return Math.min(count0, count1);
+
+    }
+
+}

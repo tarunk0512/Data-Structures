4bff962107f5c8d19f11719333115af28899aadc
Tarun Kashyap <tarunk05151999@gmail.com>
2025-09-27 20:23:43 +0530

    LinkedList : Reverse Linked List at a given point from end : Edge cases covering

--- DS/src/Practice/LinkedList/DeleteNthNodeFromEnd.java
+++ DS/src/Practice/LinkedList/DeleteNthNodeFromEnd.java
@@ -8,6 +8,11 @@ public class DeleteNthNodeFromEnd {
         head = removeNthNode(head, 3);
         System.out.println();
         cr.printLinkedList(head);
+
+        Node head2 = cr.createLinkedList(java.util.List.of(1,2,3,4,5,6,7));
+        head2 = removeNthNodeEdgeCases(head2, 7);
+        System.out.println();
+        cr.printLinkedList(head2);
     }
     // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
 
@@ -28,12 +33,45 @@ public class DeleteNthNodeFromEnd {
             slow = slow.next;
             fast = fast.next;
         }
-        Node temp = slow.next.next;
-        slow.next = temp;
+        slow.next = slow.next.next;
 
         return head;
 
     }
+    // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+
+//Remove N-th Node from End
+
+    public static Node removeNthNodeEdgeCases(Node head, int n){
+
+        Node dummy = new Node(-1);
+        dummy.next = head;
+        Node slow = dummy;
+        Node fast = dummy;
+
+        if(head == null) return null;
+
+        for(int i = 0; i<=n; i++){
+            if(fast == null) {
+                return head;
+            }
+            fast = fast.next;
+        }
+
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+
+    }
+
+
+
+
 
 
 

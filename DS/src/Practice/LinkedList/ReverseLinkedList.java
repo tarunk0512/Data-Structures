29e0115f0bd00632dfda75de6ac1d5fea215a29f
Tarun Kashyap <tarunk05151999@gmail.com>
2025-09-27 14:38:36 +0530

    LinkedList : Reverse Linked List - Practice

--- DS/src/Practice/LinkedList/ReverseLinkedList.java
+++ DS/src/Practice/LinkedList/ReverseLinkedList.java
@@ -10,14 +10,21 @@ public class ReverseLinkedList {
         Node head = cr.createLinkedList(list);
         cr.printLinkedList(head);
         System.out.println();
-        Node newHead = getReverseLinkedList(head);
+        Node newHead = getReversedLinkedList(head);
         cr.printLinkedList(newHead);
+
+        Node reverseAtIndex = getReveredAtIndex(newHead, 4);
+        System.out.println();
+        cr.printLinkedList(reverseAtIndex);
     }
     // 1->2->3->4
     // 1->2->3->4
-    public static Node getReverseLinkedList(Node head){
+    // Revese Linked List
+
+    public static Node getReversedLinkedList(Node head){
         Node prev = head;
         Node curr = head.next;
+
         while(curr != null){
             Node temp = curr.next;
             curr.next = prev;
@@ -25,8 +32,39 @@ public class ReverseLinkedList {
             curr = temp;
 
         }
-        head.next =  null;
+        head.next = null;
         head = prev;
-        return head;
+    return head;
+
     }
+
+
+    public static Node getReveredAtIndex(Node head, int index){
+        Node dummy = new Node<>(null);
+        dummy.next = head;
+        Node beforeStart = dummy;
+
+        for(int i = 0; i<index; i++){
+            beforeStart = beforeStart.next;
+        }
+
+        Node sublistHead = beforeStart.next; // node at index
+        Node prev = null;
+        Node curr = sublistHead;
+
+        // Reverse the rest of the list
+        while (curr != null) {
+            Node temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        // Reconnect
+        beforeStart.next = prev;   // connect node at index to reversed tail
+        sublistHead.next = null;   // old head of sublist becomes new tail
+
+        return dummy.next;
+    }
+
 }

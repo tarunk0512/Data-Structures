964fd04266e83bd8c13b26aa423ce5c2d734c67b
Tarun Kashyap <tarunk05151999@gmail.com>
2025-09-27 20:04:41 +0530

    LinkedList : Reverse Linked List at a given point from end

---
+++ DS/src/Practice/LinkedList/DeleteNthNodeFromEnd.java
@@ -0,0 +1,46 @@
+package Practice.LinkedList;
+
+public class DeleteNthNodeFromEnd {
+    public static void main(String [] args){
+        CreateLinkedList cr = new CreateLinkedList();
+        Node head = cr.createLinkedList(java.util.List.of(1,2,3,4,5,6,7));
+        cr.printLinkedList(head);
+        head = removeNthNode(head, 3);
+        System.out.println();
+        cr.printLinkedList(head);
+    }
+    // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+
+//Remove N-th Node from End
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+    public static Node removeNthNode(Node head, int n){
+
+        Node slow = head;
+        Node fast = head;
+
+        while(n>0){
+            fast = fast.next;
+            n--;
+        }
+
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        Node temp = slow.next.next;
+        slow.next = temp;
+
+        return head;
+
+    }
+
+
+
+
+
+}
+
+
+
+
+

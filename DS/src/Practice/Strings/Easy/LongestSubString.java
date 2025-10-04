5bc2d00d5f698343c170f14fad1973e62ac379e6
Tarun Kashyap <tarunk05151999@gmail.com>
2025-10-04 19:23:15 +0530

    String : Medium Practice

--- DS/src/Practice/Strings/Easy/LongestSubString.java
+++ DS/src/Practice/Strings/Easy/LongestSubString.java
@@ -7,24 +7,44 @@ public class LongestSubString {
     public static void main(String[] args){
         String s = "abcdaefgh";
         System.out.println(longestSubString(s));
+        System.out.println(getLongestSubString2(s));
     }
     public static int longestSubString(String s){
         Set<Character> st = new HashSet<>();
         int max = 0;
         int left = 0, right = 0;
 
-        while(right < s.length()){
-            char c = s.charAt(right);
-            if(!st.contains(c)){
-                st.add(c);
-                right++;
-                max = Math.max(max, right-left+1); // a b c d a e
-            }else{
-                st.remove(c);
-                left++;
-            }
-        }
+       while(right < s.length()){
+           while(st.contains(s.charAt(right))){
+               st.remove(s.charAt(left));
+               left++;
+           }
+           st.add(s.charAt(right));
+           max = Math.max(max, right - left + 1);
+           right ++;
+       }
         return max;
 
+    }
+    //Longest SubString
+// Input : abcdabcefgabc;
+// Output : 6 - efgabc
+
+
+    public static int getLongestSubString2(String s){
+        int left = 0;
+        int max = 0;
+        Set<Character> set = new HashSet<>();
+        //set.add(s.charAt(0));
+
+        for(int right = 0; right < s.length(); right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
     }
 }
